00966282015f78015e9075ba4da62b03b5722d52
montherzbon <montherakhaled@gmail.com>
2026-09-07 16:55:17 +0300

    Update RockPaprerScissors.py

--- RockPaprerScissors.py
+++ RockPaprerScissors.py
@@ -4,23 +4,23 @@ def rps():
 
         c_choice =random.choice(moves)
 
-        playerchoice = input("choose a move (rock) (paper) (scissors): ")
+        playerchoice = input("choose a move (rock) (paper) (scissors): ").lower()
 
         if playerchoice in moves:
                 print(f"computer move: {c_choice}")
         
                 if playerchoice == "rock" and c_choice == "scissors":
-                        print("u won")
+                        print("you won")
                 elif playerchoice == "scissors" and c_choice == "rock":
                         print("computer won")
                 elif playerchoice == "paper" and c_choice == "scissors":
                         print("computer won")
                 elif playerchoice == "scissors" and c_choice == "paper":
-                        print("u won")
+                        print("you won")
                 elif playerchoice == "rock" and c_choice == "paper":
                         print("computer won")
                 elif playerchoice == "paper" and c_choice == "rock":
-                        print("u won")
+                        print("you won")
                 if  playerchoice == c_choice:
                         print("draw")
 
